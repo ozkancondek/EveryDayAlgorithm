a6ebe307521040711b0a3f6632b280d13a6886e3
ozkancondek <ozkncndek@gmail.com>
2022-09-09 16:29:13 +0200

    infinity

---
+++ PE25-1000-digit-fibonacci.js
@@ -0,0 +1,13 @@
+const findNumber = ( )=>{
+    const phi = 1.618
+    let counter =  4725
+ 
+        const num = Math.round(((phi**counter-(-1*phi)**(-1*counter)))/(5**0.5))
+        
+     
+ 
+}
+    
+console.log(findNumber())
+
+ 
